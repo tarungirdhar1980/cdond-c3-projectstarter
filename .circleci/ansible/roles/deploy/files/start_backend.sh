4850638ff9e4ecc82bce059c4d7286dbc52fbe51
tarungirdhar1980 <86316947+tarungirdhar1980@users.noreply.github.com>
2021-07-13 22:19:44 +0530

    Add files via upload

---
+++ .circleci/ansible/roles/deploy/files/start_backend.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ENVIRONMENT=production
+NODE_ENV=production
+TYPEORM_CONNECTION=postgres
+TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+TYPEORM_HOST=postgres.cslksjoz5psg.us-west-2.rds.amazonaws.com
+TYPEORM_PORT=5432
+TYPEORM_USERNAME=postgres
+TYPEORM_PASSWORD=test1234
+TYPEORM_DATABASE=postgres
+
+nohup node /home/ubuntu/dist/main.js &
