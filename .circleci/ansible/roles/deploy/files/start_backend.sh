3757d61183267ffce78d4a5fbef1ef00e6384a10
tarungirdhar1980 <86316947+tarungirdhar1980@users.noreply.github.com>
2021-07-14 22:24:11 +0530

    Update start_backend.sh

--- .circleci/ansible/roles/deploy/files/start_backend.sh
+++ .circleci/ansible/roles/deploy/files/start_backend.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+export ENVIRONMENT=production
+export NODE_ENV=production
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_ENTITIES=./modules/domain/*/*/*.entity.js
+export TYPEORM_HOST=postgres.cslksjoz5psg.us-west-2.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=test1234
+export TYPEORM_DATABASE=postgres
 
-echo "environment variable is $TYPEORM_ENTITIES"
 nohup node /home/ubuntu/dist/main.js &
